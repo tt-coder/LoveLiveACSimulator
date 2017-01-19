d86e3f8dc5ba9b7a65e9394bdc1766f04e4bc039
tt-coder <tokio.tm@gmail.com>
2017-01-19 23:52:41 +0900

    add notemove and fix button position

--- Assets/Scripts/NoteCreator.cs
+++ Assets/Scripts/NoteCreator.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class NoteCreator : MonoBehaviour {
 
+	public GameObject singleNote;
+	private GameObject obj;
+
 	// Use this for initialization
 	void Start () {
-		
+		obj = Instantiate(singleNote);
+		obj.transform.parent = GameObject.Find("NoteParent").transform;
+		obj.transform.position = new Vector3(0,3.6f,0);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/NoteMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMove : MonoBehaviour {
+
+	private float gameTime;
+	private float posX,posY,speed = 3;
+	// Use this for initialization
+	void Start () {
+		gameTime = 0;
+		//iTween.MoveTo(gameObject,iTween.Hash("x",-2.9f,"y",-3.45f,"time",1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveNote();
+	}
+
+	private void moveNote(){
+		gameTime += Time.deltaTime;
+		posX = -gameTime * speed;
+		posY = 2.0862f * posX + 2.6f;
+		gameObject.transform.localPosition = new Vector3(posX,posY,0);
+
+	}
+
+}
