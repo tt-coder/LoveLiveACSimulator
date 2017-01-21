fc013b72a8be47e19391e4d258097620b10ce1d3
tt-coder <tokio.tm@gmail.com>
2017-01-21 17:37:56 +0900

    add note destroy position and layer controll

--- Assets/Scripts/NoteMove.cs
+++ Assets/Scripts/NoteMove.cs
@@ -9,8 +9,10 @@ public class NoteMove : MonoBehaviour {
 	public int laneValue;
 	private float rad;
 
-	private float[] posXList = new float[9] {-7.65f,-7.0677f,-5.4094f,-2.928f,0f,2.928f,5.4094f,7.0677f,7.65f};
-	private float[] posYList = new float[9] {3.6f,0.672f,-1.809f,-3.468f,-4.05f,-3.468f,-1.809f,0.672f,3.6f};
+	private float[] posXList = new float[9] {-9f,-8.31492f,-6.364f,-3.444f,0f,3.444f,6.364f,8.31492f,9f};
+	private float[] posYList = new float[9] {3.6f,0.15585f,-2.764f,-4.715f,-5.4f,-4.715f,-2.764f,0.15585f,3.6f};
+	private float[] idealPosXList = new float[9] {-7.65f,-7.0677f,-5.4094f,-2.928f,0f,2.928f,5.4094f,7.0677f,7.65f};
+	private float[] idealPosYList = new float[9] {3.6f,0.672f,-1.809f,-3.468f,-4.05f,-3.468f,-1.809f,0.672f,3.6f};
 
 	void Start () {
 		gameTime = 0;
@@ -23,7 +25,7 @@ public class NoteMove : MonoBehaviour {
 	}
 
 	private void moveNote2(){
-		iTween.MoveTo(gameObject,iTween.Hash("position",new Vector3(posXList[laneValue],posYList[laneValue],0f),"easeType","linear","time",1.0f,"oncomplete","desroyNote"));
+		iTween.MoveTo(gameObject,iTween.Hash("position",new Vector3(posXList[laneValue],posYList[laneValue],0f),"easeType","linear","time",2.0f,"oncomplete","desroyNote"));
 		iTween.ScaleTo(gameObject,iTween.Hash("x",0.4f,"y",0.4f,"time",1.0f,"easeType","easeOutSine"));
 	}
 
---
+++ Assets/Scripts/SingleNote.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleNote : MonoBehaviour {
+
+	private int lane;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setLayer();
+	}
+
+	void setLayer(){
+		lane = GetComponent<NoteMove>().laneValue;
+		Debug.Log(lane);
+		if(lane != 0 && lane != 8){
+			transform.SetSiblingIndex((int)transform.localPosition.y);
+		}else{
+			transform.SetSiblingIndex((int)transform.localPosition.x);
+		}
+	}
+}
